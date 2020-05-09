70a3adb39035e2416bd2e5d8f28ff9a223066b78
Om Mahesh <2ommahesh@gmail.com>
2020-05-09 02:01:39 +0000

    Update player.js

--- web_html/player.js
+++ web_html/player.js
@@ -6,10 +6,10 @@ class Player {
     this.pos = createVector(x, y);
     this.size = blockSize;
     
-    this.vel = createVector(0, 0);
+    this.vel = createVector(scrollSpeed, 0);
     this.grav = createVector(0, 0.6);
     this.jumpCount = 0;
-    this.jumping = false;
+    this.spin = false;
     this.rotate = 0;
     
     this.behind = false;
@@ -18,6 +18,7 @@ class Player {
   }
 
   draw() {
+    
     switch (theme) {
         
       case "yellow":
@@ -29,16 +30,10 @@ class Player {
         break;
         
     }
-    
     push();
     rectMode(CENTER);
     translate(this.pos.x + this.size/2, this.pos.y + this.size/2);
     rotate(this.rotate);
-    if (this.jumping) {
-      this.rotate -= PI/16;
-    } else {
-      this.rotate = 0;
-    }
     rect(0, 0, this.size, this.size);
     pop();
     
@@ -46,22 +41,43 @@ class Player {
 
   jump() {
     if (this.jumpCount < 1) {
-      this.vel = createVector(0, 10);
+      this.vel = createVector(scrollSpeed, 10);
       this.jumpCount++;
-      this.jumping = true;
+      this.spin = true;
     }
   }
 
   update() {
+    
+    if (this.status == "dead") {
+      level = new Level(level.levelNum);
+    }
     
     this.pPos = this.pos.copy();
 
     this.vel.sub(this.grav);
     this.pos.add(this.vel);
     
-    if (this.pos.x < 100 && !this.behind) {
-      this.pos.x += 1;
+    if (this.pos.x < scroll || this.pos.y < 0) {
+      this.status = "dead";
     }
+    
+    if (this.pos.x < scroll + 100) {
+      scrollSpeed = slowScroll;
+      if (!this.behind) {
+        this.pos.x += 1;
+      }
+    } else {
+      scrollSpeed = normScroll;
+    }
+    
+    if (this.spin) {
+      this.rotate -= PI/16;
+    } else {
+      this.rotate = 0;
+    }
+    
+    this.spin = true;
     
     for (let i = 0; i < level.obstacles.length; i++) {
       
@@ -75,22 +91,24 @@ class Player {
             
             this.behind = false;
 
-            if (this.pPos.y - currObj.pos.y > abs(this.pPos.x - currObj.pos.x) && this.pPos.y > currObj.pos.y) {
+            if (this.pPos.y - currObj.pos.y > abs(this.pPos.x - currObj.pos.x) && this.pPos.y > currObj.pos.y && this.pPos.x + this.size > currObj.pos.x && this.pPos.x < currObj.pos.x + blockSize) {
               
               this.pos.y = currObj.pos.y + blockSize;
               this.vel.y = 0;
               this.jumpCount = 0;
-              this.jumping = false;
+              this.spin = false;
 
-            } else if (currObj.pos.y - this.pPos.y > abs(this.pPos.x - currObj.pos.x) && currObj.pos.y > this.pPos.y) {
+            } else if (currObj.pos.y - this.pPos.y > abs(this.pPos.x - currObj.pos.x) && currObj.pos.y > this.pPos.y && this.pPos.x + this.size > currObj.pos.x && this.pPos.x < currObj.pos.x + blockSize) {
 
               this.pos.y = currObj.pos.y - blockSize;
               this.vel.y = 0;
+              this.spin = false;
 
             } else if (currObj.pos.x - this.pPos.x > abs(this.pPos.y - currObj.pos.y) && currObj.pos.x > this.pPos.x) {
               
               this.pos.x = currObj.pos.x - blockSize;
               this.behind = true;
+              this.spin = false;
               
             }
             break;
@@ -103,7 +121,7 @@ class Player {
             
           case "Spike":
             
-            page = "menu";
+            this.status = "dead";
             break;
           
         }
