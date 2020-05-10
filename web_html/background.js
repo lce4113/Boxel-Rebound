9a91b19d9def193f244e2de349adeec249e3f33d
Om Mahesh <2ommahesh@gmail.com>
2020-05-10 01:03:09 +0000

    Add big level buttons

--- web_html/background.js
+++ web_html/background.js
@@ -62,10 +62,16 @@ class Background {
       [30, 130], // Start
       [50, 100], // Start
       [60, 60] // Start
-    ]
+    ];
 
     this.menuPadding = 20;
     this.homeSize = 40;
+    
+    this.volume = createSlider(0, 0.5, 0.1, 0);
+    this.volume.hide();
+    this.speakerSize = 20;
+    this.speakerPos = createVector(width/2-75-this.speakerSize*1/2, this.menuPadding+this.homeSize/2);
+    this.volume.position(this.speakerPos.x+this.speakerSize*2, this.speakerPos.y-this.speakerSize*1/2);
 
     this.layer1Width = 0;
     this.layer2Width = 0;
@@ -81,7 +87,7 @@ class Background {
   }
 
   drawBack() {
-    
+
     switch (page) {
 
       case "menu":
@@ -89,33 +95,39 @@ class Background {
         break;
 
       case "level1":
-        this.scene1();
+        this.scene();
         break;
 
       case "level2":
-        this.scene2();
+        this.scene();
         break;
 
       case "level20":
-        this.scene20();
+        this.scene();
         break;
 
     }
 
   }
-  
+
   drawFront() {
-    
+
     if (settings) {
       this.settings();
+    } else {
+      this.volume.hide();
     }
-    
+
     this.buttons();
 
   }
 
-  scene1() {
+  scene() {
 
+    push();
+    resetMatrix();
+    scale(1, -1);
+    translate(0, -height);
     // Background depending on color scheme
     switch (theme) {
 
@@ -166,171 +178,13 @@ class Background {
       rect(this.pos.x + xWidth, this.pos.y, this.layer2[i][0], this.layer2[i][1]);
       xWidth += this.layer2[i][0];
     }
-
-  }
-
-  scene2() {
-
-    // Background depending on color scheme
-    switch (theme) {
-
-      case "yellow":
-        background(255, 202, 40);
-        break;
-
-      case "pink":
-        background(255);
-        break;
-
-    }
-
-    // layer 1
-    switch (theme) {
-
-      case "yellow":
-        fill(255, 160, 0);
-        break;
-
-      case "pink":
-        fill(240, 207, 216);
-        break;
-
-    }
-    this.pos.x = (this.x - 0.5 * scroll) % (this.layer1Width / 2);
-
-    for (let i = 0, xWidth = 0; i < this.layer1.length; i++) {
-      rect(this.pos.x + xWidth, this.pos.y, this.layer1[i][0], this.layer1[i][1]);
-      xWidth += this.layer1[i][0];
-    }
-
-    // layer 2
-    switch (theme) {
-
-      case "yellow":
-        fill(255, 111, 0);
-        break;
-
-      case "pink":
-        fill(236, 172, 191);
-        break;
-
-    }
-    this.pos.x = (this.x - 0.6 * scroll) % (this.layer2Width / 2);
-
-    for (let i = 0, xWidth = 0; i < this.layer2.length; i++) {
-      rect(this.pos.x + xWidth, this.pos.y, this.layer2[i][0], this.layer2[i][1]);
-      xWidth += this.layer2[i][0];
-    }
-
-  }
-
-  scene20() {
-
-    // Background depending on color scheme
-    switch (theme) {
-
-      case "yellow":
-        background(255, 202, 40);
-        break;
-
-      case "pink":
-        background(255);
-        break;
-
-    }
-
-    // layer 1
-    switch (theme) {
-
-      case "yellow":
-        fill(255, 160, 0);
-        break;
-
-      case "pink":
-        fill(240, 207, 216);
-        break;
-
-    }
-    this.pos.x = (this.x - 0.5 * scroll) % (this.layer1Width / 2);
-
-    for (let i = 0, xWidth = 0; i < this.layer1.length; i++) {
-      rect(this.pos.x + xWidth, this.pos.y, this.layer1[i][0], this.layer1[i][1]);
-      xWidth += this.layer1[i][0];
-    }
-
-    // layer 2
-    switch (theme) {
-
-      case "yellow":
-        fill(255, 111, 0);
-        break;
-
-      case "pink":
-        fill(236, 172, 191);
-        break;
-
-    }
-    this.pos.x = (this.x - 0.6 * scroll) % (this.layer2Width / 2);
-
-    for (let i = 0, xWidth = 0; i < this.layer2.length; i++) {
-      rect(this.pos.x + xWidth, this.pos.y, this.layer2[i][0], this.layer2[i][1]);
-      xWidth += this.layer2[i][0];
-    }
+    pop();
 
   }
 
   menu() {
 
-    // Background depending on color scheme
-    switch (theme) {
-
-      case "yellow":
-        background(255, 202, 40);
-        break;
-
-      case "pink":
-        background(255);
-        break;
-
-    }
-
-    // layer 1
-    switch (theme) {
-
-      case "yellow":
-        fill(255, 160, 0);
-        break;
-
-      case "pink":
-        fill(240, 207, 216);
-        break;
-
-    }
-    this.pos.x = (this.x - 0.5 * scroll) % (this.layer1Width / 2);
-
-    for (let i = 0, xWidth = 0; i < this.layer1.length; i++) {
-      rect(this.pos.x + xWidth, this.pos.y, this.layer1[i][0], this.layer1[i][1]);
-      xWidth += this.layer1[i][0];
-    }
-
-    // layer 2
-    switch (theme) {
-
-      case "yellow":
-        fill(255, 111, 0);
-        break;
-
-      case "pink":
-        fill(236, 172, 191);
-        break;
-
-    }
-    this.pos.x = (this.x - 0.6 * scroll) % (this.layer2Width / 2);
-
-    for (let i = 0, xWidth = 0; i < this.layer2.length; i++) {
-      rect(this.pos.x + xWidth, this.pos.y, this.layer2[i][0], this.layer2[i][1]);
-      xWidth += this.layer2[i][0];
-    }
+    this.scene();
 
     // borders
     switch (theme) {
@@ -349,8 +203,10 @@ class Background {
     rect(400-this.menuPadding, 0, this.menuPadding, 340-this.menuPadding);
     rect(0, 340-this.menuPadding, 400, 60+this.menuPadding);
 
+    push();
+    resetMatrix();
     // levels
-    pop();
+    rectMode(CENTER);
     textAlign(CENTER, CENTER);
     for (let i = 0; i < 5; i++) {
       for (let j = 0; j < 4; j++) {
@@ -382,15 +238,20 @@ class Background {
 
           }
         }
-        rect(60 + i * 60, 120 + j * 60, 40, 40, 10);
+        if (abs(mouseX - (i*60+80)) <= 20 && abs(mouseY - (j*60+140)) <= 20) {
+          rect(80 + i * 60, 140 + j * 60, 50, 50, 10);
+          textSize(35);
+        } else {
+          rect(80 + i * 60, 140 + j * 60, 40, 40, 10);
+          textSize(25);
+        }
         fill(255);
-        textSize(25);
         text(str(1 + i + j * 5), 80 + i * 60, 140 + j * 60);
       }
     }
-    
+    rectMode(CORNER);
+
     // Menu title
-    // Title color
     switch (theme) {
 
       case "yellow":
@@ -404,22 +265,46 @@ class Background {
     }
     textSize(40);
     text("Main Menu", 200, 30+this.menuPadding/2);
+    pop();
 
   }
 
   settings() {
     
+    push();
+    resetMatrix();
+    
     fill(0,0,0,200);
-    rect(0, 200, 400, 200);
+    rect(0, 0, 400, 200);
+    
+    // Volume Slider + Icon
+    fill(255);
+    rect(this.speakerPos.x-this.speakerSize, this.speakerPos.y-this.speakerSize*3/10, this.speakerSize*3/5, this.speakerSize*3/5);
+    triangle(this.speakerPos.x-this.speakerSize, this.speakerPos.y, this.speakerPos.x, this.speakerPos.y-this.speakerSize*4/5, this.speakerPos.x, this.speakerPos.y+this.speakerSize*4/5);
+    noFill();
+    stroke(255);
+    strokeWeight(this.speakerSize*1/5);
+    if (this.volume.value() == 0) {
+      line(this.speakerPos.x+this.speakerSize*1/5, this.speakerPos.y+this.speakerSize*1/5, this.speakerPos.x+this.speakerSize*3/5, this.speakerPos.y-this.speakerSize*1/5);
+      line(this.speakerPos.x+this.speakerSize*3/5, this.speakerPos.y+this.speakerSize*1/5, this.speakerPos.x+this.speakerSize*1/5, this.speakerPos.y-this.speakerSize*1/5);
+    } else {
+      for (let i = 1; i <= ceil(this.volume.value()*6); i++) {
+        arc(this.speakerPos.x, this.speakerPos.y, this.speakerSize*4/5*i, this.speakerSize*4/5*i, -PI/3, PI/3);
+      }
+    }
+
+    this.volume.show();
+    masterVolume(this.volume.value());
+    
+    pop();
 
   }
 
   buttons() {
-    
+
     push();
-    scale(1, -1);
-    translate(0, -height);
-    
+    resetMatrix();
+
     // Home button
     // Home box color
     switch (theme) {
@@ -520,8 +405,9 @@ class Background {
       rotate(TAU/8);
       square(0, this.homeSize*3/8, this.homeSize*5/32, 2);
     }
+    rectMode(CORNER);
     pop();
-    
+
   }
-  
+
 }
